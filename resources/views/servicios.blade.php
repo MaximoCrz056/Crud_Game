72106355f4089da05b05a0d0a1111b8b7057d38f
MaximoCrz056 <maximogomezcruz@gmail.com>
2024-07-29 00:22:48 -0600

    js n popperjs

--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="{{asset('imagnes/logo2.png')}}" >
+    <link rel="shortcut icon" href="{{asset('imagnes/logo2.png')}}">
     <title>Game Zone</title>
     <link rel="stylesheet" href="vista/Bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="vista/css/estilos.css">
@@ -28,10 +29,11 @@
             transition: 0.5s;
         }
 
-        html, body {
+        html,
+        body {
             height: auto;
             width: auto;
-            
+
         }
 
         #titulo {
@@ -88,85 +90,100 @@
             opacity: 1;
         }
 
-        .card-title, .card-text {
+        .card-title,
+        .card-text {
             color: #000000;
             background-color: #ffffff9d;
             padding: 10px;
             border-radius: 5px;
         }
 
-        .bg-green { background-color: green; }
-        .bg-blue { background-color: blue; }
-        .bg-red { background-color: red; }
-        .bg-yellow { background-color: #555a13; }
-        .bg-purple { background-color: purple; }
-        .bg-orange { background-color: orange; }
-        .bg-pink { background-color: #c34f62; }
-        .bg-teal { background-color: #7045068f; }
+        .bg-green {
+            background-color: green;
+        }
 
-    </style> 
+        .bg-blue {
+            background-color: blue;
+        }
+
+        .bg-red {
+            background-color: red;
+        }
+
+        .bg-yellow {
+            background-color: #555a13;
+        }
+
+        .bg-purple {
+            background-color: purple;
+        }
+
+        .bg-orange {
+            background-color: orange;
+        }
+
+        .bg-pink {
+            background-color: #c34f62;
+        }
+
+        .bg-teal {
+            background-color: #7045068f;
+        }
+    </style>
 </head>
-<body>
-   <!-- Barra de navegacion -->  
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid" >
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation" style="background-color: black;">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <img src="{{asset('imagenes/logo2.png')}}" alt="logo" style="width: 100px;" style="background-color: black;"&nbsp;&nbsp;>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="{{url('/')}}" style="color: white;">&nbsp;&nbsp;&nbsp;Game Zone &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{url('/')}}" style="color: white;"><i class="fa-solid fa-house">&nbsp;</i>Inicio &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{url('/consol')}}" style="color: white;"><i class="fa-solid fa-gamepad">&nbsp;</i>Consolas&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{url('/videoj')}}" style="color: white;"><i class="fa-solid fa-gamepad">&nbsp;</i>Videojuegos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{url('/servicios')}}" style="color: white;"><i class="fa-solid fa-ghost">&nbsp;</i>Servicios</a>
-              </li>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              @if (Route::has('login'))
-                            <nav class="-mx-3 flex flex-1">
-                                @auth
-                                    <a
-                                        href="{{ url('/dashboard') }}"
-                                        class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Dashboard
-                                    </a>
-                                @else  
-                                
-                                     <a
-                                        href="{{ route('login') }}"
-                                        class="rounded-md px-5 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Iniciar Sesion
-                                    </a>
 
-                                    @if (Route::has('register'))
-                                    
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md px-5 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Registrarse
-                                        </a>
-                                    @endif
-                                @endauth                        
-            </ul>
-          </div>
+<body>
+    <!-- Barra de navegacion -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation" style="background-color: black;">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <img src="{{asset('imagenes/logo2.png')}}" alt="logo" style="width: 100px;" style="background-color: black;" &nbsp;&nbsp;>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <a class="navbar-brand" href="{{url('/')}}" style="color: white;">&nbsp;&nbsp;&nbsp;Game Zone &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{url('/')}}" style="color: white;"><i class="fa-solid fa-house">&nbsp;</i>Inicio &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{url('/consol')}}" style="color: white;"><i class="fa-solid fa-gamepad">&nbsp;</i>Consolas&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{url('/videoj')}}" style="color: white;"><i class="fa-solid fa-gamepad">&nbsp;</i>Videojuegos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{url('/servicios')}}" style="color: white;"><i class="fa-solid fa-ghost">&nbsp;</i>Servicios</a>
+                    </li>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    @if (Route::has('login'))
+                    <nav class="-mx-3 flex flex-1">
+                        @auth
+                        <a href="{{ url('/dashboard') }}" class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                            Dashboard
+                        </a>
+                        @else
+
+                        <a href="{{ route('login') }}" class="rounded-md px-5 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                            Iniciar Sesion
+                        </a>
+
+                        @if (Route::has('register'))
+
+                        <a href="{{ route('register') }}" class="rounded-md px-5 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                            Registrarse
+                        </a>
+                        @endif
+                        @endauth
+                </ul>
+            </div>
         </div>
-      </nav>
-      @endif
-      <!--Fin de Barra de navegacion -->  
-  
+    </nav>
+    @endif
+    <!--Fin de Barra de navegacion -->
+
     <div class="container">
         <h1 id="titulo">Nuestros juegos más populares</h1>
         <div class="row">
@@ -177,7 +194,7 @@
                         <h5 class="card-title">Renta Xbox</h5>
                         <p class="card-text">
                             2 controles $20.00 (por 1 hora) <br>
-                            1 control  $15.00 (por 1 hora) 
+                            1 control $15.00 (por 1 hora)
                         </p>
                     </div>
                 </div>
@@ -189,7 +206,7 @@
                         <h5 class="card-title">Renta PlayStation</h5>
                         <p class="card-text">
                             2 controles $20.00 (por 1 hora) <br>
-                            1 control  $15.00 (por 1 hora) 
+                            1 control $15.00 (por 1 hora)
                         </p>
                     </div>
                 </div>
@@ -211,7 +228,7 @@
                     <img src="{{asset('imagenes/arlogo.jpg')}}" class="image card-img-top" alt="Mortal Kombat 11">
                     <div class="card-body layer">
                         <h5 class="card-title">Renta Arcade</h5>
-                        <p class="card-text"> 
+                        <p class="card-text">
                             3 vidas $1.00 <br>
                             5 vidas $1.00
                         </p>
@@ -222,7 +239,8 @@
                 <div class="card bg-yellow">
                     <img src="{{asset('imagenes/mesaFut.jpg')}}" class="image card-img-top" alt="FC24">
                     <div class="card-body layer">
-                        <br><h5 class="card-title">Renta Futbolito</h5>
+                        <br>
+                        <h5 class="card-title">Renta Futbolito</h5>
                         <p class="card-text">
                             7 pelotas $1.00 <br>
                             5 pelotas $1.00
@@ -238,7 +256,7 @@
                         <p class="card-text">
                             Bolsa de chetos $10.00 <br>
                             Bolsa de papas $10.00 <br>
-                            Sopa maruchan $15.00 
+                            Sopa maruchan $15.00
                         </p>
                     </div>
                 </div>
@@ -263,17 +281,17 @@
                     <div class="card-body layer">
                         <h5 class="card-title">Venta de pizza</h5>
                         <p class="card-text">
-                            Rebanada de pizza hawaiana $15.00 <br> 
+                            Rebanada de pizza hawaiana $15.00 <br>
                             Rebanada de pizza Peperoni $15.00
                         </p>
                     </div>
                 </div>
             </div>
         </div>
-    </div> 
-     
-    <!-- Pie de pagina -->  
-    <footer class="text-center bg-body-tertiary" style="background-color: black;">    
+    </div>
+
+    <!-- Pie de pagina -->
+    <footer class="text-center bg-body-tertiary" style="background-color: black;">
         <div class="text-center p-3" style="background-color: #000000; color: white;">
             <a data-mdb-ripple-init class="btn btn-link btn-floating btn-lg text-body m-1" href="#!" role="button" data-mdb-ripple-color="dark">
                 <i class="fab fa-facebook-f" style="color: white;"></i>
@@ -293,8 +311,9 @@
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <script src="vista/Bootstrap/js/bootstrap.min.js"></script>
 </body>
+
 </html>
