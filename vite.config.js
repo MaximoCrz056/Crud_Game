87e88fd27f869bd3d938de621bb46aaa158ff2ba
Maximo Gómez Cruz <77859736+MaximoCrz056@users.noreply.github.com>
2024-07-28 23:57:26 -0600

    Update vite.config.js
    Se mamaron

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,19 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                //css
                 'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/css/bootstrap.css',
                 'resources/css/bootstrap.min.css',
+                //js
+                'resources/js/app.js',
                 'resources/js/bootstrap.min.js',
                 'resources/js/bootstrap.js',
                 'resources/js/estiloBoton.js',
                 'resources/js/estilosForm.js',
+                //sass
+                'resources/sass/_variables.scss',
+                'resources/sass/app.scss'
             ],
             refresh: true,
         }),
